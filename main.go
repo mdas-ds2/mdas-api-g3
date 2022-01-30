284d8ff72ab0f74c2755c839c8c6a949692de219
Pere Padial <pere.padial@vassit.co.uk>
2022-01-30 19:50:30 +0100

    refactor(pokemon): I/O console

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"log"
 
 	pokemonUseCases "github.com/mdas-ds2/mdas-api-g3/pokemons/pokemon/application"
+	console "github.com/mdas-ds2/mdas-api-g3/pokemons/pokemon/infrastructure/console"
 	pokeApi "github.com/mdas-ds2/mdas-api-g3/pokemons/pokemon/infrastructure/poke-api"
 )
 
 func main() {
-	// TODO: Separate this responsability
-	pokemonNameInput := flag.String("getPokemonTypes", "", "Get pokemon types passing the pokemon name")
-	flag.Parse()
+	pokemonNameInput := console.NewInputParameter("getPokemonTypes", "Get pokemon types passing the pokemon name")
 
 	pokeApiRepository := pokeApi.PokeApiPokemonRepository{}
 
@@ -20,11 +17,11 @@ func main() {
 		PokemonRepository: pokeApiRepository,
 	}
 
-	pokemonTypes, errorOnGetPokemonTypes := getTypesByName.Execute(*pokemonNameInput)
+	pokemonTypes, errorOnGetPokemonTypes := getTypesByName.Execute(pokemonNameInput)
 
 	if errorOnGetPokemonTypes != nil {
 		log.Fatalln(errorOnGetPokemonTypes)
 	}
 
-	fmt.Println(pokemonTypes)
+	console.Print(pokemonTypes)
 }
---
+++ pokemons/pokemon/infrastructure/console/ConsoleOutput.go
@@ -0,0 +1,8 @@
+package console
+
+import "fmt"
+
+func Print(value string) {
+	fmt.Println(value)
+
+}
