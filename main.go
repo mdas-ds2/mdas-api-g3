05161feb58ee32965793cffd313cc57b23cf621b
Pere Padial <pere.padial@vassit.co.uk>
2022-02-03 21:01:06 +0100

    refactor(pokemon-types): main refactor

--- main.go
+++ main.go
@@ -1,27 +1,10 @@
 package main
 
 import (
-	"log"
-
-	console "github.com/mdas-ds2/mdas-api-g3/generic/infrastructure/console"
-	pokemonTypeUseCases "github.com/mdas-ds2/mdas-api-g3/pokemon-types/application"
-	pokeApi "github.com/mdas-ds2/mdas-api-g3/pokemon-types/infrastructure/poke-api"
+	command "github.com/mdas-ds2/mdas-api-g3/pokemon-types/infrastructure/command"
 )
 
 func main() {
-	pokemonNameInput := console.NewInputParameter("getPokemonTypes", "Get pokemon types passing the pokemon name")
-
-	pokeApiPokemonTypeRepository := pokeApi.PokeApiPokemonTypesRepository{}
-
-	getByPokemonName := pokemonTypeUseCases.GetByPokemonName{
-		PokemonTypeRepository: pokeApiPokemonTypeRepository,
-	}
-
-	pokemonTypes, errorOnGetPokemonTypes := getByPokemonName.Execute(pokemonNameInput)
-
-	if errorOnGetPokemonTypes != nil {
-		log.Fatalln(errorOnGetPokemonTypes)
-	}
-
-	console.Print(pokemonTypes[0].GetName())
+	getTypesByNameCommand := command.GetTypesByNameCommand{}
+	getTypesByNameCommand.Run()
 }
