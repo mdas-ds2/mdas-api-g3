4ae6ae2053f2924fd8db9d958e5e158500cbd081
EzeGo <ezequiel.gomez@nexplore.com>
2022-02-08 16:16:23 +0100

    refactor (types): exception created; api url fixed

--- pokemons/pokemon-types/infrastructure/poke-api/PokeApiPokemonTypesRepository.go
+++ pokemons/pokemon-types/infrastructure/poke-api/PokeApiPokemonTypesRepository.go
@@ -1,22 +1,22 @@
 package pokeApi
 
 import (
-	"errors"
-
 	http "github.com/mdas-ds2/mdas-api-g3/generic/infrastructure/http"
+	webServer "github.com/mdas-ds2/mdas-api-g3/generic/infrastructure/web-server"
 	pokemonTypes "github.com/mdas-ds2/mdas-api-g3/pokemons/pokemon-types/domain"
 )
 
 type PokeApiPokemonTypesRepository struct{}
 
-const pokeApiUrl = "http://127.0.0.1/"
+const pokeApiUrl = "https://pokeapi.co/api/v2/pokemon/"
 
 func (pokeApiPokemonTypesRepository PokeApiPokemonTypesRepository) FindByPokemonName(pokemonName pokemonTypes.PokemonName) (pokemonTypes.PokemonTypes, error) {
 	urlPath := pokeApiUrl + pokemonName.GetValue()
 	response, statusCode, errorOnResponse := http.Get(urlPath)
 
 	if statusCode == http.SERVICE_UNAVAILABLE {
-		return pokemonTypes.PokemonTypes{}, errors.New("Service unavailable")
+		serviceUnavailableException := webServer.CreateServiceUnavailableException()
+		return pokemonTypes.PokemonTypes{}, serviceUnavailableException.GetError()
 	}
 
 	if errorOnResponse != nil {
@@ -24,7 +24,7 @@ func (pokeApiPokemonTypesRepository PokeApiPokemonTypesRepository) FindByPokemon
 	}
 
 	if statusCode == http.NOT_FOUND {
-		pokemonNotFoundException := pokemonTypes.CreatePokemonNotFoundException(pokemonName.GetValue())
+		pokemonNotFoundException := pokemonTypes.CreatePokemonNotFoundException(pokemonName)
 		return pokemonTypes.PokemonTypes{}, pokemonNotFoundException.GetError()
 	}
 
