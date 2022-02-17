113c915cbad2a7b82d6a3fc3f9035cb1e39f45ab
Pere Padial <perepadial@gmail.com>
2022-02-17 21:16:29 +0100

    feature (pokemon): application implementation

---
+++ src/pokemons/pokemon/application/GetPokemonDetails.go
@@ -0,0 +1,29 @@
+package pokemon
+
+import (
+	"fmt"
+
+	domain "github.com/mdas-ds2/mdas-api-g3/src/pokemons/pokemon/domain"
+)
+
+type GetPokemonDetails struct {
+	repository domain.Repository
+}
+
+func (getPokemonDetails *GetPokemonDetails) Execute(pokemonId int) (PokemonDetails, error) {
+	id := domain.CreateId(pokemonId)
+	pokemon, error := getPokemonDetails.repository.Find(id)
+	//has to return a domain exception
+	if error != nil {
+		return PokemonDetails{}, error
+	}
+
+	pokemonDetail := PokemonDetails{
+		pokemon.GetId().GetValue(),
+		pokemon.GetName().GetValue(),
+		pokemon.GetHeight().GetValue(),
+		pokemon.GetWeight().GetValue(),
+	}
+	fmt.Printf(pokemonDetail.Name, error)
+	return pokemonDetail, nil
+}
---
+++ src/pokemons/pokemon/application/PokemonDetails.go
@@ -0,0 +1,8 @@
+package pokemon
+
+type PokemonDetails struct {
+	Id     int
+	Name   string
+	Height int
+	Weight int
+}
